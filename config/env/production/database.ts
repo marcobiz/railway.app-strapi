c2fe7fef32ebeaa17baf4cc2cdabcd36381bfa81
marcobiz <marcobiz@hotmail.it>
2025-01-28 12:21:44 +0100

    edit

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -14,7 +14,7 @@ export default ({ env }) => ({
       schema: env('DATABASE_SCHEMA', 'public'),
     },
     options: {
-      autoMigration: true, // 🔑 Questo è fondamentale!
+      autoMigration: true, // 🔑 Questo è fondamentale!!
       pool: {
         min: env.int('DATABASE_POOL_MIN', 2),
         max: env.int('DATABASE_POOL_MAX', 10)
